6b147a230992bf838156ae8bd7fa9b4efeb98d47
jpmikkers <jean.paul.mikkers@gmail.com>
2025-10-31 13:24:28 +0100

    wrap into a js class, experiment with autosizing

--- BaksteenXTerm/XTerm.razor.js
+++ BaksteenXTerm/XTerm.razor.js
@@ -3,6 +3,69 @@ import { Terminal } from './js/xterm.esm.js';
 import { FitAddon } from "./js/addon-fit.esm.js";
 import { WebLinksAddon } from './js/addon-web-links.esm.js';
 
+export class XTermInteropHelper {
+    #terminal;
+    #fitAddon;
+    #webLinksAddon
+    #resizeObserver;
+
+    constructor(elementRef) {
+        //alert("Initializing terminal...");
+        this.#terminal = new Terminal();
+        this.#fitAddon = new FitAddon();
+        this.#webLinksAddon = new WebLinksAddon();
+        this.#terminal.loadAddon(this.#fitAddon);
+        this.#terminal.loadAddon(this.#webLinksAddon);
+
+        //terminal.open(document.getElementById(containerId));
+        this.#terminal.open(elementRef);
+        this.#fitAddon.fit();
+
+        this.#resizeObserver = new ResizeObserver(() => {
+            const dims = this.#fitAddon.proposeDimensions();
+            if (dims.cols < this.#terminal.cols) {
+                // width is shrinking, so resize to minimum first to avoid issues
+                this.#terminal.resize(10, this.#terminal.rows);
+            }
+            this.#fitAddon.fit();
+        });
+        this.#resizeObserver.observe(elementRef);
+    }
+
+    fit() {
+        this.#fitAddon.fit();
+    }
+
+    write(data) {
+        this.#terminal.write(data);
+    }
+
+    clear() {
+        this.#terminal.clear();
+    }
+
+    reset() {
+        this.#terminal.reset();
+    }
+
+    blur() {
+        this.#terminal.blur();
+    }
+
+    focus() {
+        this.#terminal.focus();
+    }
+
+    dispose() {
+        this.#resizeObserver.disconnect();
+        this.#terminal.dispose();
+    }
+}
+
+export function createXTermInteropHelper(elementRef) {
+    return new XTermInteropHelper(elementRef);
+}
+
 export function initTerminal(elementRef, dotNetRef) {
 
     //alert("Initializing terminal...");
